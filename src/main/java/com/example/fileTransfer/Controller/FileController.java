b5aeb920b507f3788e473304a91112c49c15357c
Manish Verma <manishvrm8877@gmail.com>
2026-02-03 21:32:26 +0530

    Initial Comment

---
+++ src/main/java/com/example/fileTransfer/Controller/FileController.java
@@ -0,0 +1,21 @@
+package com.example.fileTransfer.Controller;
+
+import com.example.fileTransfer.Service.FileService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class FileController {
+
+    private final FileService fileService;
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @GetMapping("/get-path")
+    public String getPath() throws IOException {
+        return fileService.copyFile();
+    }
+}
---
+++ src/main/java/com/example/fileTransfer/Service/FileService.java
@@ -0,0 +1,22 @@
+package com.example.fileTransfer.Service;
+
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+@Service
+public class FileService {
+    public String copyFile() throws IOException {
+
+        Path source = Paths.get("/Users/manishverma/Downloads/demo-3.zip");
+        Path target = Paths.get("/Users/manishverma/Documents/demo-3.zip");
+
+        Files.copy(source,target,StandardCopyOption.REPLACE_EXISTING);
+       
+        return "File copied";
+
+    }
+}
